ec9b1da8b29d523a2ef6762d6ef602716966b783
mmmamaj <gokusaiajyn06@gmail.com>
2026-09-14 22:41:59 -0300

    Add files via upload

---
+++ assets/js/social.js
@@ -0,0 +1,2 @@
+// A implementação ativa da rede social está em reels.js e usa a database por meio de /api/social.
+// Este arquivo é mantido apenas para compatibilidade com versões antigas do projeto.
