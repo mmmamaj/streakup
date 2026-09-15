01987eb3fd73ecf60b43a4c08e81c7399dc1abf1
mmmamaj <gokusaiajyn06@gmail.com>
2026-09-14 22:34:35 -0300

    main

--- social.js
+++
@@ -1,2 +0,0 @@
-// A implementação ativa da rede social está em reels.js e usa a database por meio de /api/social.
-// Este arquivo é mantido apenas para compatibilidade com versões antigas do projeto.
